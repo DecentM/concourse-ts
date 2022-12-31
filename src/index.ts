1ffcb9b2d86eb3f2d09534744d8957554cab9f12
DecentM <decentm@decentm.com>
2022-12-31 22:49:47 +0200

    feat: export Initer type and Utils

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,10 @@ export * from './utils/with-try'
 import {Pipeline} from './components/pipeline'
 import {Task} from './components/task'
 
+export {Initer} from './declarations/initialisable'
+
 export * as Type from './declarations/types'
+export * as Utils from './utils'
 
 export * from './components/pipeline'
 export * from './components/resource'
