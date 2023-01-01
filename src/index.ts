01f5331dba6cb2f1b3ab3312a0ff732d5ba9d2fd
DecentM <decentm@decentm.com>
2023-01-01 02:11:36 +0200

    chore: export AnyStep and Job from package index

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from './utils/with-try'
 import {Pipeline} from './components/pipeline'
 import {Task} from './components/task'
 
+export {AnyStep} from './components/step'
 export {Initer} from './declarations/initialisable'
 
 export * as Type from './declarations/types'
@@ -14,6 +15,7 @@ export * from './components/pipeline'
 export * from './components/resource'
 export * from './components/resource-type'
 export * from './components/task'
+export * from './components/job'
 
 export const compile = (input: Pipeline | Task) => {
   return YAML.stringify(input.serialise())
