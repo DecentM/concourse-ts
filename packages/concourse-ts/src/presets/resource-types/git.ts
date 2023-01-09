a35aa663fa7f25adf055f5aab39bfe93724593d8
DecentM <decentm@decentm.com>
2023-01-09 03:23:54 +0200

    fix: git name is arbitrary, type is not

--- packages/concourse-ts/src/presets/resource-types/git.ts
+++ packages/concourse-ts/src/presets/resource-types/git.ts
@@ -2,8 +2,8 @@ import {ResourceType} from '../../components/resource-type'
 import {get_duration} from '../../utils'
 
 export class Git extends ResourceType {
-  constructor() {
-    super('git')
+  constructor(name: string) {
+    super(name)
 
     this.set_check_every(get_duration({hours: 24}))
     this.type = 'git'
--- packages/concourse-ts/src/utils/type-of.spec.ts
+++ packages/concourse-ts/src/utils/type-of.spec.ts
@@ -4,28 +4,35 @@ import {type_of} from './type-of'
 test('null', (t) => {
   t.is(type_of(null), 'null')
 })
+
 test('undefined', (t) => {
   t.is(type_of(undefined), 'undefined')
 })
+
 test('number', (t) => {
   t.is(type_of(1), 'number')
   t.is(type_of(NaN), 'number')
 })
+
 test('string', (t) => {
   t.is(type_of(''), 'string')
   t.is(type_of('a'), 'string')
 })
+
 test('boolean', (t) => {
   t.is(type_of(true), 'boolean')
   t.is(type_of(false), 'boolean')
 })
+
 test('array', (t) => {
   t.is(type_of([1]), 'array')
 })
+
 test('object', (t) => {
   t.is(type_of({}), 'object')
   t.is(type_of(new Number(1)), 'object')
 })
+
 test('function', (t) => {
   t.is(
     type_of(() => null),
@@ -38,9 +45,11 @@ test('function', (t) => {
     'function'
   )
 })
+
 test('symbol', (t) => {
   t.is(type_of(Symbol('a')), 'symbol')
 })
+
 test('bigint', (t) => {
   t.is(type_of(BigInt('0001')), 'bigint')
 })
