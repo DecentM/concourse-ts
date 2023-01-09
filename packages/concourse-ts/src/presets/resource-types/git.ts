1e93981747e328ee15c6dac4a5db5f1268510a16
DecentM <decentm@decentm.com>
2023-01-09 03:18:00 +0200

    fix: make git resource have 'git' for its name

--- packages/concourse-ts/src/presets/resource-types/git.ts
+++ packages/concourse-ts/src/presets/resource-types/git.ts
@@ -2,8 +2,8 @@ import {ResourceType} from '../../components/resource-type'
 import {get_duration} from '../../utils'
 
 export class Git extends ResourceType {
-  constructor(name: string) {
-    super(name)
+  constructor() {
+    super('git')
 
     this.set_check_every(get_duration({hours: 24}))
     this.type = 'git'
