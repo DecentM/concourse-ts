539310a92b2142ddcb8db1f844b832d5604f81e3
DecentM <decentm@decentm.com>
2023-01-14 01:33:21 +0200

    chore(cli): remove unused imports

--- packages/concourse-ts/src/cli/app/index.tsx
+++ packages/concourse-ts/src/cli/app/index.tsx
@@ -1,4 +1,4 @@
-import React, {FunctionComponent, useEffect, useState} from 'react'
+import React, {FunctionComponent} from 'react'
 import {render} from 'ink'
 
 import {CompileCommand, CompileProps} from './commands/compile'
