ac278eebddd26e2da59ca35f2ab589ca5ac3ca91
DecentM <decentm@decentm.com>
2023-01-14 05:52:30 +0200

    feat: add declarations for build metadata for template strings

---
+++ packages/concourse-ts/src/declarations/metadata.ts
@@ -0,0 +1,12 @@
+export const BuildMetadata = {
+  BuildId: '$BUILD_ID',
+  BuildName: '$BUILD_NAME',
+  BuildJobName: '$BUILD_JOB_NAME',
+  BuildPipelineName: '$BUILD_PIPELINE_NAME',
+  BuildPipelineInstanceVars: '$BUILD_PIPELINE_INSTANCE_VARS',
+  BuildTeamName: '$BUILD_TEAM_NAME',
+  BuildCreatedBy: '$BUILD_CREATED_BY',
+  AtcExternalUrl: '$ATC_EXTERNAL_URL',
+} as const
+
+export type BuildMetadata = typeof BuildMetadata[keyof typeof BuildMetadata]
--- packages/concourse-ts/src/index.ts
+++ packages/concourse-ts/src/index.ts
@@ -1,12 +1,8 @@
-import * as YAML from 'yaml'
-
 export * from './utils/with-try'
-
-import {Pipeline} from './components/pipeline'
-import {Task} from './components/task'
 
 export * from './components/step'
 export {Initer} from './declarations/initialisable'
+export {BuildMetadata} from './declarations/metadata'
 
 export * as Type from './declarations/types'
 export * as Utils from './utils'
