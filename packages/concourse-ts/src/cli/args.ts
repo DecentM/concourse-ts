875509636d28cae764e777f40d6f1680460507c7
DecentM <decentm@decentm.com>
2023-01-29 15:18:44 +0200

    refactor(concourse-ts/cli): move internal args parsing implementation from yargs to meow

--- packages/concourse-ts/src/cli/args.ts
+++ packages/concourse-ts/src/cli/args.ts
@@ -1,39 +1,61 @@
-import yargs from 'yargs/yargs'
-import {hideBin} from 'yargs/helpers'
+import meow from 'meow'
+import path from 'node:path'
+
 import {AppCommand, AppProps} from './app'
 
 export const parseProps = async (argv: string[]): Promise<AppProps> => {
-  const parsed = await yargs(hideBin(argv))
-    .command('compile', 'compile pipelines', (yargs) => {
-      return yargs
-        .option('input', {
+  const cliName = path.parse(process.argv0).name
+
+  const cli = meow(
+    `
+      Usage
+        $ ${cliName} [command] [flags]
+
+      Options
+        --input, -i             Glob of .ts files containing a default export of a function that returns a Pipeline
+        --output-directory, -o  The directory to output all pipelines to
+        --extract-tasks, -e     If set, tasks within the pipeline will be extracted and replaced with a file reference
+
+      Examples
+        $ ${cliName} compile -i ci/pipelines/*.ts -o .ci
+
+        $ ${cliName} compile -i ./**/*.pipeline.ts -e
+    `,
+    {
+      importMeta: {
+        url: path.resolve(__dirname, '../../package.json'),
+      },
+
+      argv,
+
+      flags: {
+        'input': {
+          type: 'string',
           alias: 'i',
+          isRequired: true,
+        },
+
+        'output-directory': {
           type: 'string',
-          description:
-            'Glob of .ts files containing a default export of a function that returns a Pipeline',
-          demandOption: '-i is required',
-        })
-        .option('output-directory', {
           alias: 'o',
-          type: 'string',
-          description: 'The directory to output all pipelines to',
           default: '.',
-        })
-        .option('extract-tasks', {
-          alias: 'e',
+        },
+
+        'extract-tasks': {
           type: 'boolean',
-          description:
-            'If set, tasks within the pipeline will be extracted and replaced with a file reference',
+          alias: 'e',
           default: false,
-        })
-    })
-    .parse()
+        },
+      },
+    }
+  )
 
   return {
-    command: parsed._[0] as AppCommand,
+    command: cli.input[0] as AppCommand,
     options: {
-      input: parsed.input,
-      output_directory: parsed.outputDirectory,
+      input: cli.flags.input,
+      output_directory: cli.flags.outputDirectory,
+      extract_tasks: cli.flags.extractTasks,
     },
   }
 }
