a2432e82725c73edbfcabd4b489f4598a65899c6
DecentM <decentm@decentm.com>
2023-01-30 08:09:25 +0200

    feat(r-time): implement source types and release v0.1.0

--- packages/concourse-ts-resource-time/src/resource.spec.ts
+++ packages/concourse-ts-resource-time/src/resource.spec.ts
@@ -6,7 +6,7 @@ import { TimeResourceType } from './resource-type'
 test('stores name', (t) => {
   const rt = new TimeResourceType('my-time-resource_type')
   const r = new TimeResource('my-time-resource', rt, {
-    // TODO: Fill this out
+    interval: '5m',
   })
 
   t.is(r.name, 'my-time-resource')
