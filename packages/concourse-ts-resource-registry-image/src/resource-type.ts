b16d3d5136c82ab7ad94bcf0f2898ff4cb5d3513
DecentM <decentm@decentm.com>
2023-02-01 08:02:17 +0200

    feat(r-registry-image): implement source types and release v0.1.0

--- packages/concourse-ts-resource-registry-image/src/resource-type.ts
+++ packages/concourse-ts-resource-registry-image/src/resource-type.ts
@@ -1,19 +1,25 @@
 import { ResourceType, Type } from '@decentm/concourse-ts'
 
 export type Input = {
-  tag?: string
+  /**
+   * https://hub.docker.com/r/concourse/registry-image-resource/tags
+   */
+  tag: string
 }
 
 export class RegistryImageResourceType extends ResourceType {
-  constructor(name: string, input?: Input, init?: Type.Initer<RegistryImageResourceType>) {
+  constructor(
+    name: string,
+    input: Input,
+    init?: Type.Initer<RegistryImageResourceType>
+  ) {
     super(`${name}_type`)
 
     this.type = 'registry-image'
 
-    // TODO: Fill this out
     this.source = {
-      repository: '',
-      tag: input?.tag ?? '',
+      repository: 'concourse/registry-image-resource',
+      tag: input.tag,
     }
 
     if (init) {
