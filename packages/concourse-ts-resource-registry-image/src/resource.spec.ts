fd738e5e9f463c4ddb79c96d7dc1f1c84d7fdb63
DecentM <decentm@decentm.com>
2023-02-01 08:02:17 +0200

    feat(r-registry-image): implement source types and release v0.1.0

--- packages/concourse-ts-resource-registry-image/src/resource.spec.ts
+++ packages/concourse-ts-resource-registry-image/src/resource.spec.ts
@@ -4,9 +4,12 @@ import { RegistryImageResource } from './resource'
 import { RegistryImageResourceType } from './resource-type'
 
 test('stores name', (t) => {
-  const rt = new RegistryImageResourceType('my-registry-image-resource_type')
+  const rt = new RegistryImageResourceType('my-registry-image-resource_type', {
+    tag: 'alpine',
+  })
+
   const r = new RegistryImageResource('my-registry-image-resource', rt, {
-    // TODO: Fill this out
+    repository: '',
   })
 
   t.is(r.name, 'my-registry-image-resource')
