bc2f7f7bd2f509ccaf8bdc8e3d124bb702243d2f
DecentM <decentm@decentm.com>
2023-02-26 23:41:51 +0200

    feat(r-keyval): implement typings

--- packages/concourse-ts-resource-keyval/src/index.ts
+++ packages/concourse-ts-resource-keyval/src/index.ts
@@ -1,2 +1,23 @@
-export * from './resource-type'
-export * from './resource'
+import * as ConcourseTs from '@decentm/concourse-ts'
+import * as RegistryImage from '@decentm/concourse-ts-resource-registry-image'
+
+/**
+ * https://github.com/SWCE/keyval-resource
+ */
+export type Source = Record<string, never>
+
+export type PutParams = {
+  /**
+   * The properties file to read the key values from
+   */
+  file: string
+}
+
+export type GetParams = Record<string, never>
+
+export type Resource = ConcourseTs.Resource<Source, PutParams, GetParams>
+
+export type ResourceType = ConcourseTs.ResourceType<
+  'registry-image',
+  RegistryImage.Source<'swce/keyval-resource'>
+>
