5686f19cca14c7a5e9423c066cae728650bf7f81
DecentM <decentm@decentm.com>
2023-02-26 22:57:39 +0200

    refactor(r-apache-directory-index): convert to pure typings format

--- packages/concourse-ts-resource-apache-directory-index/src/index.ts
+++ packages/concourse-ts-resource-apache-directory-index/src/index.ts
@@ -1,2 +1,41 @@
-export * from './resource-type'
-export * from './resource'
+import * as ConcourseTs from '@decentm/concourse-ts'
+import * as RegistryImage from '@decentm/concourse-ts-resource-registry-image'
+
+/**
+ * https://github.com/cloudlena/apache-directory-index-resource
+ */
+export type Source = {
+  /**
+   * The path of where the your versioned folders reside
+   */
+  directory: string
+  /**
+   * Describes the pattern of how the folders are named (use $VERSION as a
+   * placeholder for the version)
+   */
+  folder_pattern: string
+  /**
+   * Describes the pattern of where the file resides and how it's named (use
+   * $VERSION as a placeholder for the version)
+   */
+  file_pattern: string
+  /**
+   * If true then the downloaded file will be expanded into the "expanded"
+   * directory
+   */
+  expand?: boolean
+}
+
+export type PutParams = never
+
+/**
+ * No get params, pass an empty object
+ */
+export type GetParams = Record<string, never>
+
+export type Resource = ConcourseTs.Resource<Source, PutParams, GetParams>
+
+export type ResourceType = ConcourseTs.ResourceType<
+  'registry-image',
+  RegistryImage.Source<'mastertinner/apache-directory-index-resource'>
+>
