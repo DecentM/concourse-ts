db54ddb6d98a59c861b786b31e9858fce9ddc2a7
DecentM <decentm@decentm.com>
2023-02-27 00:43:02 +0200

    feat(r-curl): implement curl resource

---
+++ packages/concourse-ts-resource-curl/src/index.ts
@@ -0,0 +1,71 @@
+import * as ConcourseTs from '@decentm/concourse-ts'
+import * as RegistryImage from '@decentm/concourse-ts-resource-registry-image'
+
+/**
+ * https://github.com/DecentM/concourse-curl-resource
+ */
+export type Source = {
+  /**
+   * The address this resource will interact with. Any URL accepted by `curl` is
+   * allowed.
+   */
+  url: string
+  /**
+   * An array of strings to pass to `curl`. For the best results, wrap each in
+   * double quotes. Example:
+   * ```yaml
+   * arguments:
+   *   - "-A"
+   *   - "My-User-Agent"
+   * ```
+   */
+  arguments?: string[]
+  /**
+   * Same as `arguments`, but this will only be passed during the `check`
+   * behaviour.
+   */
+  check_arguments?: string[]
+  /**
+   * An object that defines a range of response codes. If the response code of
+   * the URL is within this range, the request will be considered successful.
+   */
+  response_code?: {
+    /**
+     * Default: 200.
+     *
+     * The lower bound of the acceptable range
+     */
+    min?: number
+    /**
+     * Default: 299.
+     *
+     * The upper bound of the acceptable range
+     */
+    max?: number
+  }
+}
+
+export type PutParams = {
+  /**
+   * An additional array of strings that serve as arguments. This array will be
+   * merged with the arguments from the source configuration, `check_arguments`
+   * is ignored.
+   */
+  arguments?: string[]
+}
+
+export type GetParams = {
+  /**
+   * An additional array of strings that serve as arguments. This array will be
+   * merged with the arguments from the source configuration, `check_arguments`
+   * is ignored.
+   */
+  arguments?: string[]
+}
+
+export type Resource = ConcourseTs.Resource<Source, PutParams, GetParams>
+
+export type ResourceType = ConcourseTs.ResourceType<
+  'registry-image',
+  RegistryImage.Source<'decentm/concourse-curl-resource'>
+>
