814765d0a4cb2034fafa3a5286e38bdf5e1b1c30
DecentM <decentm@decentm.com>
2023-02-28 20:23:11 +0200

    fix(r-registry-image): add resource-type typing

--- packages/concourse-ts-resource-registry-image/src/index.ts
+++ packages/concourse-ts-resource-registry-image/src/index.ts
@@ -250,3 +250,8 @@ export type GetParams = {
 }
 
 export type Resource = ConcourseTs.Resource<Source, PutParams, GetParams>
+
+export type ResourceType = ConcourseTs.ResourceType<
+  'registry-image',
+  Source<'concourse/registry-image-resource'>
+>
