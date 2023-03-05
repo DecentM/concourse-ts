5ed77bd3b5d9d35d806d981645a55b122edd99c8
DecentM <decentm@decentm.com>
2023-03-05 23:33:41 +0200

    test: implement some tests for the compile command

--- packages/concourse-ts/src/cli/commands/compile/index.spec.ts
+++ packages/concourse-ts/src/cli/commands/compile/index.spec.ts
@@ -1,9 +1,9 @@
 import anyTest, {TestFn} from 'ava'
 import path from 'path'
 import {tmpName} from 'tmp-promise'
-import rimraf from 'rimraf'
 
 import {Compile} from '.'
+import VError from 'verror'
 
 const test = anyTest as TestFn<{tmp_dir: string}>
 
@@ -13,10 +13,6 @@ test.beforeEach('temp directory', async (t) => {
   t.context = {
     tmp_dir: tmp_path,
   }
-})
-
-test.afterEach('remove test directory', async (t) => {
-  await rimraf(t.context.tmp_dir)
 })
 
 test('compiles pipeline', async (t) => {
@@ -33,7 +29,7 @@ test('compiles pipeline', async (t) => {
   let output_count = 0
 
   compile.on('output', (file) => {
-    t.log(`Compilation succeeded, written to: ${file}`)
+    t.log(file)
     output_count++
   })
 
@@ -42,4 +38,44 @@ test('compiles pipeline', async (t) => {
   })
 
   await compile.run()
+})
+
+test('does not compile when export is not a function', async (t) => {
+  const compile = new Compile({
+    input: path.join(__dirname, 'test-data/exports-number.pipeline.ts'),
+    extract_tasks: false,
+    output_directory: t.context.tmp_dir,
+  })
+
+  let error: VError | null = null
+
+  compile.on('error', (event_error) => {
+    error = event_error
+  })
+
+  compile.on('output', () => {
+    t.fail('output written from invalid pipeline')
+  })
+
+  compile.on('end', () => {
+    t.assert(
+      error?.message.includes(
+        'failed validation. Make sure your glob only resolves to Typescript files with a default export that returns a Pipeline instance.'
+      )
+    )
+  })
+
+  await compile.run()
+})
+
+test('refuses to compile if input matches no files', async (t) => {
+  const compile = new Compile({
+    input: 'test-data/good.pipeline.ts',
+    extract_tasks: false,
+    output_directory: t.context.tmp_dir,
+  })
+
+  await t.throwsAsync(() => compile.run(), {
+    message: 'Glob input "test-data/good.pipeline.ts" matched no files',
+  })
 })
