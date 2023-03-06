9ec6509f53d75e139674cc55e11641ccdfc3774b
DecentM <decentm@decentm.com>
2023-03-06 19:19:28 +0200

    test: 100% coverage for the import command

--- packages/concourse-ts/src/cli/commands/import/index.spec.ts
+++ packages/concourse-ts/src/cli/commands/import/index.spec.ts
@@ -1,6 +1,7 @@
 import anyTest, {TestFn} from 'ava'
 import path from 'path'
 import {tmpName} from 'tmp-promise'
+import {mkdirp} from 'mkdirp'
 
 import {Import} from '.'
 
@@ -21,10 +22,6 @@ test('compiles pipeline', async (t) => {
     output_directory: t.context.tmp_dir,
   })
 
-  import_command.on('error', (error) => {
-    t.fail(error.stack)
-  })
-
   let output_count = 0
 
   import_command.on('output', (file) => {
@@ -37,4 +34,56 @@ test('compiles pipeline', async (t) => {
   })
 
   await import_command.run()
+})
+
+test('refuses to write into existing output direcotry', async (t) => {
+  await mkdirp(t.context.tmp_dir)
+
+  const compile = new Import({
+    input: path.join(__dirname, 'test-data/good.yml'),
+    package_path: '@decentm/concourse-ts',
+    output_directory: t.context.tmp_dir,
+  })
+
+  compile.on('error', (event_error) => {
+    t.assert(
+      event_error.message.includes(
+        'Output directory already exists, refusing to override'
+      )
+    )
+  })
+
+  await compile.run()
+})
+
+test('refuses inputs that match no files', async (t) => {
+  const compile = new Import({
+    input: './no-file.yml',
+    package_path: '@decentm/concourse-ts',
+    output_directory: t.context.tmp_dir,
+  })
+
+  compile.on('error', (event_error) => {
+    t.is(event_error.message, 'Glob input "./no-file.yml" matched no files')
+  })
+
+  await compile.run()
+})
+
+test('refuses inputs that match corrupt yml files', async (t) => {
+  const compile = new Import({
+    input: path.join(__dirname, 'test-data/corrupt.yml'),
+    package_path: '@decentm/concourse-ts',
+    output_directory: t.context.tmp_dir,
+  })
+
+  compile.on('error', (event_error) => {
+    t.assert(
+      event_error.message.includes(
+        'Nested mappings are not allowed in compact mappings'
+      )
+    )
+  })
+
+  await compile.run()
 })
