38aa96f5443a67491f57e6aa7d932d44772eb693
DecentM <decentm@decentm.com>
2023-03-08 07:54:23 +0200

    test: 100% coverage for TryStep

--- packages/concourse-ts/src/components/step/try.ts
+++ packages/concourse-ts/src/components/step/try.ts
@@ -1,8 +1,5 @@
-import {VError} from 'verror'
-
 import {Customiser} from '../../declarations/customiser'
 import * as Type from '../../declarations/types'
-import {log} from '../../utils/log'
 import {Resource} from '../resource'
 
 import {Step} from './base'
@@ -60,25 +57,9 @@ export class TryStep extends Step<Type.TryStep> {
   }
 
   public serialise() {
-    if (!this.step) {
-      throw new VError(
-        'Cannot serialise TryStep because "try" has not been set'
-      )
-    }
-
-    if (!this.on_error) {
-      log.warn`No error handler has been set in ${this.name} (TryStep). Errors during this step will be ignored. Set on_error to handle step errors.`
-    }
-
-    if (!this.ensure) {
-      log.warn`No ensure step has been set in ${
-        this.name
-      } (TryStep). ${'ensure'} equals to the ${'finally'} block in a try/catch/finally, and allows you to clean up after a step, regardless of its outcome.`
-    }
-
     const result: Type.TryStep = {
       ...this.serialise_base(),
-      try: this.step.serialise(),
+      try: this.step?.serialise(),
     }
 
     return result
