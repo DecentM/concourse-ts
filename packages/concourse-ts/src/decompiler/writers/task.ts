0ca06e1736f11fe589dfd9795187dabcb9314a3a
DecentM <decentm@decentm.com>
2023-03-09 02:11:25 +0200

    refactor: prepare Resource and Task writers for testing

--- packages/concourse-ts/src/decompiler/writers/task.ts
+++ packages/concourse-ts/src/decompiler/writers/task.ts
@@ -3,83 +3,78 @@ import {Identifier} from '../../utils/identifier'
 import {parse_bytes, type_of} from '../../utils'
 
 import {write_command} from './command'
+import {empty_string_or} from '../../utils/empty_string_or'
 
 export const write_task = <Input extends Identifier, Output extends Identifier>(
   name: string,
   task: Task<Input, Output>
 ): string => {
   return `new Task(${JSON.stringify(name)}, (task) => {
-    ${
-      type_of(task.image_resource) !== 'undefined'
-        ? `task.set_image_resource(${JSON.stringify(task.image_resource)})`
-        : ''
-    }
+    ${empty_string_or(
+      task.image_resource,
+      (image_resource) =>
+        `task.set_image_resource(${JSON.stringify(image_resource)})`
+    )}
 
-    ${
-      type_of(task.platform) !== 'undefined'
-        ? `task.platform = ${JSON.stringify(task.platform)}`
-        : ''
-    }
+    ${empty_string_or(
+      task.platform,
+      (platform) => `task.platform = ${JSON.stringify(platform)}`
+    )}
 
-    ${
-      type_of(task.run) !== 'undefined'
-        ? `task.run = ${write_command(`${name}_run`, task.run)}`
-        : ''
-    }
+    ${empty_string_or(
+      task.run,
+      (run) => `task.run = ${write_command(`${name}_run`, run)}`
+    )}
 
-    ${
-      type_of(task.caches) === 'array' && task.caches.length
-        ? `task.add_cache(...${JSON.stringify(task.caches)})`
-        : ''
-    }
+    ${empty_string_or(
+      task.caches,
+      (caches) => `task.add_cache(...${JSON.stringify(caches)})`
+    )}
 
-    ${
-      type_of(task.container_limits?.cpu) !== 'undefined'
-        ? `task.set_cpu_limit_percent(${task.container_limits.cpu})`
-        : ''
-    }
+    ${empty_string_or(
+      task.container_limits?.cpu,
+      (cpu) => `task.set_cpu_limit_percent(${cpu})`
+    )}
 
-    ${
-      type_of(task.container_limits?.memory) !== 'undefined'
-        ? `task.set_memory_limit(${JSON.stringify(
-            parse_bytes(task.container_limits.memory)
-          )})`
-        : ''
-    }
+    ${empty_string_or(
+      task.container_limits?.memory,
+      (memory) => `task.set_memory_limit(${parse_bytes(memory)})`
+    )}
 
-    ${
-      type_of(task.inputs) === 'array' && task.inputs.length
-        ? `task.add_input(...${JSON.stringify(task.inputs)})`
-        : ''
-    }
+    ${empty_string_or(
+      task.inputs,
+      (inputs) =>
+        `task.add_input(${inputs
+          .map((input) => JSON.stringify(input))
+          .join(', ')})`
+    )}
 
-    ${
-      type_of(task.outputs) === 'array' && task.outputs.length
-        ? `task.add_output(...${JSON.stringify(task.outputs)})`
-        : ''
-    }
+    ${empty_string_or(
+      task.outputs,
+      (outputs) =>
+        `task.add_outputs(${outputs
+          .map((output) => JSON.stringify(output))
+          .join(', ')})`
+    )}
 
-    ${
-      task.params
-        ? Object.entries(task.params)
-            .filter(
-              ([, value]) =>
-                type_of(value) !== 'null' && type_of(value) !== 'undefined'
-            )
-            .map(([name, value]) => {
-              return `task.set_params({
+    ${empty_string_or(task.params, (params) =>
+      Object.entries(params)
+        .filter(
+          ([, value]) =>
+            type_of(value) !== 'null' && type_of(value) !== 'undefined'
+        )
+        .map(([name, value]) => {
+          return `task.set_params({
           key: ${JSON.stringify(name)},
           value: ${JSON.stringify(String(value))}
         })`
-            })
-            .join('\n')
-        : ''
-    }
+        })
+        .join('\n')
+    )}
 
-    ${
-      type_of(task.rootfs_uri) !== 'undefined'
-        ? `task.rootfs_uri = ${JSON.stringify(task.rootfs_uri)}`
-        : ''
-    }
+    ${empty_string_or(
+      task.rootfs_uri,
+      (rootfs_uri) => `task.rootfs_uri = ${JSON.stringify(rootfs_uri)}`
+    )}
   })`
 }
