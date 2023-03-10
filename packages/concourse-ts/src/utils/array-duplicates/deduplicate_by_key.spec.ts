1589bf359fbc325adedb73147c7575f5ddd5b122
DecentM <decentm@decentm.com>
2023-03-10 12:19:55 +0200

    test: fix coverage reporting in deduplicate_by_key tests

--- packages/concourse-ts/src/utils/array-duplicates/deduplicate_by_key.spec.ts
+++ packages/concourse-ts/src/utils/array-duplicates/deduplicate_by_key.spec.ts
@@ -1,10 +1,11 @@
 import test from 'ava'
 import {deduplicate_by_key} from '.'
 
-const noop = () => ''
-
 test('returns empty array for non-arrays', (t) => {
-  t.deepEqual(deduplicate_by_key(noop, null as unknown as string[]), [])
+  t.deepEqual(
+    deduplicate_by_key(() => '', null as unknown as string[]),
+    []
+  )
 })
 
 test('filters duplicates', (t) => {
