fc1810cf59b2a7163efdbc11c3f0ba766e322405
DecentM <decentm@decentm.com>
2023-03-11 22:34:43 +0200

    test: 100% coverage for resource validator

---
+++ packages/concourse-ts/src/validation/resources.spec.ts
@@ -0,0 +1,87 @@
+import test from 'ava'
+import {Identifier} from '../utils'
+import {ValidationWarning, ValidationWarningType} from '../utils/warning-store'
+
+import {validate_resources} from './resources'
+
+test('allows empty resources array', (t) => {
+  const seen_types = {}
+  const warnings = validate_resources(
+    {
+      jobs: [],
+      resources: [],
+    },
+    seen_types
+  )
+
+  t.false(warnings.has_fatal())
+  t.deepEqual(warnings.get_warnings(), [])
+})
+
+test('allows no resources', (t) => {
+  const seen_types = {}
+  const warnings = validate_resources(
+    {
+      jobs: [],
+    },
+    seen_types
+  )
+
+  t.false(warnings.has_fatal())
+  t.deepEqual(warnings.get_warnings(), [])
+})
+
+test("doesn't allow duplicate names", (t) => {
+  const seen_types = {}
+  const warnings = validate_resources(
+    {
+      jobs: [],
+      resources: [
+        {
+          name: 'a' as Identifier,
+          type: 'registry-image' as Identifier,
+          source: {},
+        },
+        {
+          name: 'a' as Identifier,
+          type: 'registry-image' as Identifier,
+          source: {},
+        },
+      ],
+    },
+    seen_types
+  )
+
+  t.true(warnings.has_fatal())
+  t.deepEqual(warnings.get_warnings(), [
+    new ValidationWarning({
+      type: ValidationWarningType.Fatal,
+      messages: ['Resource 0 and 1 have the same name: "a"'],
+    }),
+  ])
+})
+
+test("doesn't allow resources with no type", (t) => {
+  const seen_types = {}
+  const warnings = validate_resources(
+    {
+      jobs: [],
+      resources: [
+        {
+          name: 'my-resource' as Identifier,
+          type: '' as Identifier,
+          source: {},
+        },
+      ],
+    },
+    seen_types
+  )
+
+  t.true(warnings.has_fatal())
+  t.deepEqual(warnings.get_warnings(), [
+    new ValidationWarning({
+      type: ValidationWarningType.Fatal,
+      messages: ['Resource "resources.my-resource" has no type'],
+    }),
+  ])
+})
