5496d3bbbd9015d28d30c079dfbde53fec805f2e
DecentM <decentm@decentm.com>
2023-05-03 21:22:47 +0300

    test: require 100% coverage to pass tests

--- packages/concourse-ts/src/utils/resolve-across/index.spec.ts
+++ packages/concourse-ts/src/utils/resolve-across/index.spec.ts
@@ -1,10 +1,9 @@
 import test from 'ava'
 
-import {Pipeline, Step} from '../../declarations'
+import {InParallelStep, Pipeline, Step, TaskStep} from '../../declarations'
 import {Identifier} from '../identifier'
 
 import {resolve_across_pipeline} from '.'
-import {is_in_parallel_step} from '../step-type'
 
 const create_pipeline = (): Pipeline => ({
   jobs: [
@@ -47,15 +46,35 @@ test('creates the correct number of tasks', (t) => {
 
   resolve_across_pipeline(pipeline)
 
-  const step1 = pipeline.jobs[0].plan[0]
-
-  if (!is_in_parallel_step(step1)) {
-    t.fail('Did not create in_parallel step')
-    return
-  }
+  const step1 = pipeline.jobs[0].plan[0] as InParallelStep
 
   t.is(step1.across, undefined)
 
   // Should create 2 * 3 steps because across[0].values.length * across[1].values.length
   t.is((step1.in_parallel as Step[]).length, 6)
+})
+
+test('leaves non-across steps alone', (t) => {
+  const pipeline = create_pipeline()
+  const orig_pipeline = create_pipeline()
+
+  pipeline.jobs[0].plan[0].across = undefined
+  orig_pipeline.jobs[0].plan[0].across = undefined
+
+  resolve_across_pipeline(pipeline)
+
+  t.deepEqual(pipeline, orig_pipeline)
+})
+
+test('only changes variables that are defined in the matrix', (t) => {
+  const pipeline = create_pipeline()
+
+  pipeline.jobs[0].plan[0].across![0].var = 'something-else' as Identifier
+
+  resolve_across_pipeline(pipeline)
+
+  const step = pipeline.jobs[0].plan[0] as InParallelStep
+  const child_step = step.in_parallel[0] as TaskStep
+
+  t.is(child_step.task, 'running-with-node-((.:node))' as Identifier)
 })
