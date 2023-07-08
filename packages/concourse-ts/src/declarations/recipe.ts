b0043ca3da735e9484eaeba7debe99cdb7f94bb3
DecentM <decentm@decentm.com>
2023-07-08 21:43:13 +0300

    feat: allow recipes to specify their customiser inputs

--- packages/concourse-ts/src/declarations/recipe.ts
+++ packages/concourse-ts/src/declarations/recipe.ts
@@ -1,6 +1,10 @@
 import {Pipeline} from '../components'
 import {Customiser} from './customiser'
 
-export type Recipe<Group extends string = never> = (
-  customise?: Customiser<Pipeline<Group>, void>
+export type Recipe<
+  Group extends string = never,
+  CustomiserInstance = Pipeline<Group>,
+  CustomiserParent = void,
+> = (
+  customise?: Customiser<CustomiserInstance, CustomiserParent>
 ) => (pipeline: Pipeline<Group>) => void
