d21271988fab58cd5d6b64454ed57047108d74ef
DecentM <decentm@decentm.com>
2024-07-26 20:33:25 +0300

    build: move to dockerfile for building

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,25 @@
-ARG BASE_IMAGE
+ARG NODE_VERSION
 
-FROM ${BASE_IMAGE} as base
+FROM node:${NODE_VERSION}-alpine as base
 
 #########################################
 FROM base as depskeleton
 WORKDIR /app
 
 COPY package.json yarn.lock ./
-COPY ./packages/concourse-ts/package.json ./packages/concourse-ts/package.json
-
-ARG package
-COPY ./packages/${package}/package.json ./packages/${package}/package.json
 
 #########################################
 FROM depskeleton as proddeps
 WORKDIR /app
 
-RUN yarn --frozen-lockfile --non-interactive --production=true --network-concurrency=5
+RUN yarn workspaces focus -A --production --network-concurrency=5
 
 #########################################
 FROM proddeps as devdeps
 WORKDIR /app
 
 RUN apk add --no-cache python3 make gcc g++
-RUN yarn --frozen-lockfile --non-interactive --production=false --network-concurrency=5
+RUN yarn install --immutable --inline-builds
 
 #########################################
 FROM devdeps as sources
@@ -35,15 +31,19 @@ COPY . .
 FROM sources as build
 WORKDIR /app
 
-ARG package
-RUN yarn nx build ${package}
+RUN yarn moon :build
 
 #########################################
-FROM sources as test
+FROM build as test
 WORKDIR /app
 
-ARG package
-RUN yarn nx test ${package}
+RUN yarn moon #testable:test
+
+#########################################
+FROM sources as lint
+WORKDIR /app
+
+RUN yarn moon :lint
 
 #########################################
 FROM base as runtime
@@ -51,43 +51,10 @@ WORKDIR /app
 
 COPY --from=proddeps /app/node_modules ./node_modules
 
-COPY --from=build /app/dist/ ./
+COPY --from=moon /app/dist/ ./
 COPY --from=proddeps /app/packages/concourse-ts/node_modules ./packages/concourse-ts/node_modules
-
-ARG package
-COPY --from=proddeps /app/packages/${package}/node_modules ./packages/${package}/node_modules
 
 ENV PATH="${PATH}:/app/packages/concourse-ts-cli/bin"
 ENV NODE_PATH="${NODE_PATH}:/app/node_modules"
 
 ENTRYPOINT [ "sh", "-eu" ]
-
-#########################################
-ARG BASE_IMAGE
-FROM ${BASE_IMAGE} AS cli
-
-ARG concourse_ts_cli
-ARG concourse_ts
-ARG concourse_ts_recipe_auto_pipeline
-ARG concourse_ts_recipe_oci_build
-ARG concourse_ts_recipe_npm_dependencies
-ARG concourse_ts_resource_npm
-ARG concourse_ts_resource_registry_image
-ARG typescript
-
-RUN yarn global add \
-  --non-interactive \
-  --production=false \
-  --network-concurrency=5 \
-  @decentm/concourse-ts-cli@${concourse_ts_cli} \
-  @decentm/concourse-ts@${concourse_ts} \
-  @decentm/concourse-ts-recipe-auto-pipeline@${concourse_ts_recipe_auto_pipeline} \
-  @decentm/concourse-ts-recipe-oci-build@${concourse_ts_recipe_oci_build} \
-  @decentm/concourse-ts-recipe-npm-dependencies@${concourse_ts_recipe_npm_dependencies} \
-  @decentm/concourse-ts-resource-npm@${concourse_ts_resource_npm} \
-  @decentm/concourse-ts-resource-registry-image@${concourse_ts_resource_registry_image} \
-  typescript@${typescript}
-
-ENV NODE_PATH="${NODE_PATH}:/usr/local/share/.config/yarn/global/node_modules"
-
-CMD ["concourse-ts"]
--- ci/pipeline/concourse-ts.pipeline.ts
+++ ci/pipeline/concourse-ts.pipeline.ts
@@ -2,6 +2,7 @@ import '../configure.js'
 
 import * as ConcourseTs from '@decentm/concourse-ts'
 import { create_auto_pipeline } from '@decentm/concourse-ts-recipe-auto-pipeline'
+import { create_oci_build } from '@decentm/concourse-ts-recipe-oci-build'
 
 import { git_ci, git } from '../resources/git'
 
@@ -14,6 +15,31 @@ const auto_pipeline = create_auto_pipeline<Group>({
   path: `.ci/pipeline/${name}.yml`,
 })
 
+const docker = (
+  target: string,
+  extra_args: Record<string, string> = {},
+  customise?: (
+    instance: ConcourseTs.Task<
+      ConcourseTs.Utils.Identifier,
+      ConcourseTs.Utils.Identifier
+    >
+  ) => void
+) => {
+  const oci_build = create_oci_build({
+    resource: git,
+    options: {
+      target,
+      build_args: {
+        NODE_ENV: 'production',
+        NODE_VERSION: '20.15',
+        ...extra_args,
+      },
+    },
+  })
+
+  return oci_build(customise)
+}
+
 export default () => {
   return new ConcourseTs.Pipeline<Group>(
     name,
@@ -22,53 +48,57 @@ export default () => {
         new ConcourseTs.Job('checks', (job) => {
           job.add_step(git.as_get_step())
 
-          job.add_step(
-            new ConcourseTs.Task('yarn', (task) => {
-              task.add_input({ name: git.name })
-              task.add_output({ name: git.name })
+          job.add_step(new ConcourseTs.Task('build', docker('build')).as_task_step())
+          job.add_step(new ConcourseTs.Task('lint', docker('lint')).as_task_step())
+          job.add_step(new ConcourseTs.Task('test', docker('build')).as_task_step())
 
-              task.add_cache({
-                path: './.yarn/cache',
-              })
+          // job.add_step(
+          //   new ConcourseTs.Task('yarn', (task) => {
+          //     task.add_input({ name: git.name })
+          //     task.add_output({ name: git.name })
 
-              task.set_image_resource({
-                type: 'registry-image',
-                source: {
-                  repository: 'node',
-                  tag: '20.15-alpine',
-                },
-              })
+          //     task.add_cache({
+          //       path: './.yarn/cache',
+          //     })
 
-              task.run = new ConcourseTs.Command((command) => {
-                command.dir = git.name
-                command.path = '/usr/local/bin/yarn'
-                command.add_arg('--immutable')
-              })
-            }).as_task_step()
-          )
+          //     task.set_image_resource({y
+          //       type: 'registry-image',
+          //       source: {
+          //         repository: 'node',
+          //         tag: '20.15-alpine',
+          //       },
+          //     })
 
-          job.add_step(
-            new ConcourseTs.Task('lint', (task) => {
-              task.add_input({
-                name: git.name,
-              })
+          //     task.run = new ConcourseTs.Command((command) => {
+          //       command.dir = git.name
+          //       command.path = '/usr/local/bin/yarn'
+          //       command.add_arg('--immutable')
+          //     })
+          //   }).as_task_step()
+          // )
 
-              task.set_image_resource({
-                type: 'registry-image',
-                source: {
-                  repository: 'node',
-                  tag: '20.15-alpine',
-                },
-              })
+          // job.add_step(
+          //   new ConcourseTs.Task('lint', (task) => {
+          //     task.add_input({
+          //       name: git.name,
+          //     })
 
-              task.run = new ConcourseTs.Command((command) => {
-                command.dir = git.name
-                command.path = '/usr/local/bin/yarn'
-                command.add_arg('moon')
-                command.add_arg(':lint')
-              })
-            }).as_task_step()
-          )
+          //     task.set_image_resource({
+          //       type: 'registry-image',
+          //       source: {
+          //         repository: 'node',
+          //         tag: '20.15-alpine',
+          //       },
+          //     })
+
+          //     task.run = new ConcourseTs.Command((command) => {
+          //       command.dir = git.name
+          //       command.path = '/usr/local/bin/yarn'
+          //       command.add_arg('moon')
+          //       command.add_arg(':lint')
+          //     })
+          //   }).as_task_step()
+          // )
         })
       )
     })
