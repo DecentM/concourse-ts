435eb7342193d1eb3b9ac5db76f1646b88617e17
DecentM <decentm@decentm.com>
2024-08-27 02:36:29 +0300

    build: separate pack and publish

--- ci/pipelines/concourse-ts/jobs/publish.ts
+++ ci/pipelines/concourse-ts/jobs/publish.ts
@@ -7,7 +7,7 @@ import { checks_job } from './checks'
 
 import { scaffold_moon_task } from '../tasks/scaffold-moon'
 import { docker } from '../lib/docker'
-import { create_npm_resource } from 'ci/resources/npm'
+// import { create_npm_resource } from 'ci/resources/npm'
 
 const package_names = fs.readdirSync('packages')
 
@@ -23,16 +23,41 @@ export const publish_job = new ConcourseTs.Job('publish', (job) => {
     ).as_task_step()
   )
 
-  for (const package_name of package_names) {
-    const package_npm = create_npm_resource({ package: package_name })
+  job.add_step(
+    new ConcourseTs.Task('pack', (task) => {
+      task.run = new ConcourseTs.Command((command) => {
+        task.set_image_resource({
+          type: 'registry-image',
+          source: {
+            repository: 'node',
+            tag: '20.16.0-alpine',
+          },
+        })
 
-    job.add_step(
-      package_npm.as_put_step({
-        inputs: [ConcourseTs.Utils.get_identifier('image')],
-        params: {
-          path: `image/rootfs/app/packages/${package_name}/`,
-        },
+        command.dir = ConcourseTs.Utils.get_var('.:package')
+
+        command.add_arg('-exuc')
+        command.add_arg('npm pack')
       })
-    )
-  }
+    }).as_task_step((step) => {
+      step.add_across({
+        values: package_names,
+        var: ConcourseTs.Utils.get_identifier('package'),
+        max_in_flight: 5,
+      })
+    })
+  )
+
+  // for (const package_name of package_names) {
+  //   const package_npm = create_npm_resource({ package: package_name })
+
+  //   job.add_step(
+  //     package_npm.as_put_step({
+  //       inputs: [ConcourseTs.Utils.get_identifier('image')],
+  //       params: {
+  //         path: `image/rootfs/app/packages/${package_name}/`,
+  //       },
+  //     })
+  //   )
+  // }
 })
