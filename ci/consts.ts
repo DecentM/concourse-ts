26819e4fa4892067de943633cea4f3f2835ad5cd
DecentM <decentm@decentm.com>
2025-01-25 09:27:10 +0000

    chore: upgrade to Node 22

--- ci/consts.ts
+++ ci/consts.ts
@@ -1,6 +1,6 @@
 import * as ConcourseTs from '@decentm/concourse-ts'
 
-export const NODE_VERSION = '20.16.0'
+export const NODE_VERSION = '22.13.1'
 
 export const Secret = {
   npm_automation_token: ConcourseTs.Utils.get_var('npm.automation-token'),
